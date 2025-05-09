28dd202429696522a9d35db0753bb58ef1a11228
Faust VIII <joh.faustviii@gmail.com>
2025-05-09 14:41:44 +0200

    Simple code to use sync and async methods from Mutiny

--- src/main/java/joh/faust/playground/SimpleCode.java
+++ src/main/java/joh/faust/playground/SimpleCode.java
@@ -3,6 +3,8 @@ package joh.faust.playground;
 import io.smallrye.mutiny.Multi;
 import io.smallrye.mutiny.Uni;
 
+import java.time.Duration;
+
 public class SimpleCode {
 
     public static void main(String[] args) {
@@ -19,6 +21,34 @@ public class SimpleCode {
                     return index;
                 }))
                 .onItem().transform(c -> "Character: " + c)
+                .subscribe().with(System.out::println);
+
+        invokeAndCall();
+        asyncTransform();
+    }
+
+    private static void invokeAndCall() {
+        Uni.createFrom().item("Call exceution").onItem().call(() -> Uni.createFrom().item("?").onItem().delayIt().by(Duration.ofSeconds(1)))
+                .subscribe().with(System.out::println);
+
+        Uni.createFrom().item("Invoke execution").onItem().invoke(s -> System.out.println(s + " call"))
+                        .subscribe().with(System.out::println);
+
+        Uni.createFrom().item("Another call").onSubscription().invoke(() -> System.out.println("Subscribed"))
+                .subscribe().with(System.out::println);
+    }
+
+    private static void asyncTransform() {
+        Uni.createFrom().item("Async Transformed Faust")
+                .onItem().transformToUni(s -> Uni.createFrom().item("Hello " + s))
+                .subscribe().with(System.out::println);
+
+        Multi.createFrom().items(6, 1, 7)
+                .onItem().transformToUniAndMerge(i -> Uni.createFrom().item("Hello Merged " + i).onItem().delayIt().by(Duration.ofSeconds(i)))
+                .subscribe().with(System.out::println);
+
+        Multi.createFrom().items(6, 1, 7)
+                .onItem().transformToUniAndConcatenate(i -> Uni.createFrom().item("Hello Concatenated " + i).onItem().delayIt().by(Duration.ofSeconds(i)))
                 .subscribe().with(System.out::println);
     }
 }
