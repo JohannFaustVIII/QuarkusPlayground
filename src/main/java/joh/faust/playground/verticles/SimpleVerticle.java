50f2fd15ce0aaab61145a92db3ab8ad154074d7c
Faust VIII <joh.faustviii@gmail.com>
2025-05-26 15:02:50 +0200

    Add simple router with path

--- src/main/java/joh/faust/playground/verticles/SimpleVerticle.java
+++ src/main/java/joh/faust/playground/verticles/SimpleVerticle.java
@@ -3,7 +3,10 @@ package joh.faust.playground.verticles;
 import io.smallrye.mutiny.Uni;
 import io.smallrye.mutiny.vertx.core.AbstractVerticle;
 import io.vertx.core.DeploymentOptions;
+import io.vertx.core.json.Json;
 import io.vertx.mutiny.core.Vertx;
+import io.vertx.mutiny.ext.web.Router;
+import io.vertx.mutiny.ext.web.RoutingContext;
 
 public class SimpleVerticle extends AbstractVerticle {
 
@@ -13,8 +16,11 @@ public class SimpleVerticle extends AbstractVerticle {
     public Uni<Void> asyncStart() {
         vertx.setPeriodic(2000L, tick -> counter++);
 
+        Router router = Router.router(vertx);
+        router.get("/hello/:name").handler(this::getHello);
+
         return vertx.createHttpServer()
-                .requestHandler(req -> req.response().endAndForget("Hello World @" +counter))
+                .requestHandler(router)
                 .listen(8000)
                 .onItem()
                 .invoke(() -> System.out.println("Check localhost:8000"))
@@ -22,6 +28,19 @@ public class SimpleVerticle extends AbstractVerticle {
                 .invoke(Throwable::printStackTrace)
                 .replaceWithVoid();
     }
+
+    private void getHello(RoutingContext routingContext) {
+        String name = routingContext.request().getParam("name");
+
+        String response = "Hello World to " + name + "; Counter = " + counter;
+
+        routingContext.response()
+                .putHeader("content-type", "application/json")
+                .setStatusCode(200)
+                .endAndForget(Json.encodePrettily(response));
+    }
+
+
 
     public static void main(String[] args) {
         Vertx vertx = Vertx.vertx();
